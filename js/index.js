988b597c7dfb7195f62a967f3405f2fadf09b43c
jod35 <jodestrevin@gmail.com>
2020-06-22 04:59:42 -0400

    added service div and footer

--- js/index.js
+++ js/index.js
@@ -1,45 +1,43 @@
-const nav=document.querySelector('nav');
-const navbar_items=document.querySelectorAll('.nav-link');
-const logo_area=document.querySelector('.logo');
+const nav = document.querySelector("nav");
+const navbar_items = document.querySelectorAll(".nav-link");
+const logo_area = document.querySelector(".logo");
+const dropdown_menu = document.querySelector(".drop-down");
+const dropdown_link = document.getElementById("buy-btn");
 
-window.onscroll=function(){shrinkOnscroll()};
+window.onscroll = function () {
+  shrinkOnscroll();
+};
 
-function shrinkOnscroll(){
-    if (document.body.scrollTop>100|| document.documentElement.scrollTop>100){
-        nav.style.backgroundColor="rgb(219, 30, 30)";
-        nav.style.color="white";
-        nav.style.padding="10px";
+function shrinkOnscroll() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    nav.style.backgroundColor = "rgb(219, 30, 30)";
+    nav.style.color = "white";
+    nav.style.padding = "10px";
 
+    logo_area.style.color = "white";
 
-        logo_area.style.color="white";
-
-        for(let i=0; i<navbar_items.length; i++){
-            navbar_items[i].style.color="white";
-        }
+    for (let i = 0; i < navbar_items.length; i++) {
+      navbar_items[i].style.color = "white";
     }
-    else{
-        nav.style.backgroundColor="white";
-        logo_area.style.color="rgb(219, 30, 30)";
-        nav.style.padding="20px";
-        for(let i=0; i<navbar_items.length; i++){
-            navbar_items[i].style.color="rgb(219, 30, 30)";
+  } else {
+    nav.style.backgroundColor = "white";
+    logo_area.style.color = "rgb(219, 30, 30)";
+    nav.style.padding = "20px";
+    for (let i = 0; i < navbar_items.length; i++) {
+      navbar_items[i].style.color = "rgb(219, 30, 30)";
 
-            navbar_items[i].addEventListener('mouseover',()=>{
-                navbar_items[i].style.color="black";
-
-                navbar_items[i].addEventListener('mouseleave',changeToBlack);
-            });
-        }
+      navbar_items[i].addEventListener("mouseover", () => {
+        navbar_items[i].style.color = "black";
+      });
     }
+  }
 }
 
+dropdown_link.addEventListener("click", showDropDownMenu);
 
-function changeToBlack(){
-    for(let i=0; i<navbar_items.length;i++){
-        navbar_items[i].style.color="rgb(219, 30, 30)";
-    }
+function showDropDownMenu() {
+  dropdown_menu.style.display = "";
 }
-
-
-
-
