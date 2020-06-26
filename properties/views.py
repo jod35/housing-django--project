fe0376ffa2a52a179e2f594189b98a6b0d8adff1
jod35 <jodestrevin@gmail.com>
2020-06-26 02:15:35 -0400

    create the first views

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import House,WareHouse,Land
 
-# Create your views here.
+
+
+def index(request):
+    return render(request,'properties/index.html')
+
+def contacts(request):
+    return render(request,'properties/contacts.html')
+
+
+def agents(request):
+    return render(request,'properties/agents.html')
