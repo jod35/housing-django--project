4b4e256812d15b809c908e7eba6ce7b7c9ed8806
jod35 <jodestrevin@gmail.com>
2020-06-26 02:07:24 -0400

    register house,warehouse and land models to the site admin

--- properties/admin.py
+++ properties/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
+from .models import WareHouse,House,Land
+
 
 # Register your models here.
 
 #House Admin
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display=('name','price','location','agent')
+    list_filter=('uploaded','price','location')
+
+
+
+
 #WareHouse Admin
+@admin.register(WareHouse)
+class WareHouseAdmin(admin.ModelAdmin):
+    list_display=('name','location','space')
+    list_filter=('location','space','uploaded')
+
 #Land Admin
+@admin.register(Land)
+class LandAdmin(admin.ModelAdmin):
+    list_display=('location','space','agent')
+    list_filter=('uploaded','space','location')
 
