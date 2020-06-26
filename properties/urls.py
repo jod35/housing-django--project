5054e7c80c2b9b15f4cf5431fc3ccff119ab3277
jod35 <jodestrevin@gmail.com>
2020-06-26 02:24:08 -0400

    add url patterns to views

--- properties/urls.py
+++ properties/urls.py
@@ -1,3 +1,11 @@
 from . import views
 
 from django.urls import path
+
+app_name='properties'
+
+urlpatterns = [
+    path('',views.index,name='home'),
+    path('contacts',views.contacts,name='contacts'),
+    path('agents',views.agents,name='agents')
+]
