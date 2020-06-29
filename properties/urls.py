63abd51e6fc7d7fc0c5c685005b9ad62fbd52170
jod35 <jodestrevin@gmail.com>
2020-06-29 06:05:09 -0400

    created views and routes for dashboard actions

--- properties/urls.py
+++ properties/urls.py
@@ -7,11 +7,24 @@ app_name = 'properties'
 
 urlpatterns = [
     path('', views.index, name='home'),
+
     path('contacts', views.contacts, name='contacts'),
+
     path('agents', views.agents, name='agents'),
+
     path('admin-login-only', auth_views.LoginView.as_view(
         template_name='registration/login.html'), name='admin_login'),
+
     path('admin-logout', auth_views.LogoutView.as_view(
         template_name='properties/loggedout.html'), name='admin_logout'),
+
     path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
+
+    path('createhouse/', views.create_house, name="create_house"),
+
+    path('createwarehouse', views.create_warehouse, name='create_warehouse'),
+
+    path('createplot', views.create_land_plot, name='create_land_plot'),
+
+    path('register_agent', views.)
 ]
