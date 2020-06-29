1905d519445ea10d4ceaab644f4c42cde9191abe
jod35 <jodestrevin@gmail.com>
2020-06-29 06:04:56 -0400

    created views and routes for dashboard actions

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import House, WareHouse, Land
-from .models import House, WareHouse, Land
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
@@ -19,4 +19,56 @@ def agents(request):
 
 @login_required
 def admin_dashboard(request):
-    return render(request, 'properties/home.html')
+    houses = House.objects.all()
+    warehouses = WareHouse.objects.all()
+    land = Land.objects.all()
+    agents = User.objects.all()
+    context = {
+        'houses': houses,
+        'warehouses': warehouses,
+        'land_plots': land,
+        'agents': agents
+    }
+    return render(request, 'properties/home.html', context)
+
+
+@login_required
+def create_house(request):
+    context = {}
+    return render(request, 'properties/createhouse.html', context)
+
+
+@login_required
+def create_warehouse(request):
+    context = {}
+    return render(request, 'properties/createwarehouse.html', context)
+
+
+@login_required
+def create_land_plot(request):
+    context = {}
+    return render(request, 'properties/createplot.html', context)
+
+
+@login_required
+def change_house(request):
+    context = {}
+    return render(request, 'properties/changehouse.html', context)
+
+
+@login_required
+def create_agent(request):
+    context = {}
+    return render(request, 'properties/addagent.html', context)
+
+
+@login_required
+def change_warehouse(request):
+    context = {}
+    return render(request, 'properties/changewarehouse.html', context)
+
+
+@login_required
+def change_plot(request):
+    context = {}
+    return render(request, 'properties/changeplot.html', context)
